39a6a551ba2f3b1d4ad26bc36c7ed38715d05a2c
Justin Hayes <jhayes84@gmail.com>
2016-07-18 19:33:23 -0400

    Generating project from template

---
+++ src/main/kotlin/me/justinhayes/KotlinBootReactApplication.kt
@@ -0,0 +1,11 @@
+package me.justinhayes
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+open class KotlinBootReactApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(KotlinBootReactApplication::class.java, *args)
+}
